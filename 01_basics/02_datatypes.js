740fd70fea9e5520bdc9b9025f9e7acda26a002d
Rahul Chaudhary <91676034+rahulcoder2@users.noreply.github.com>
2023-12-25 03:00:31 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all Js code as newer Version.
+
+// alert( 3+ 3 ) // we are using nodejs, Not a browser
+
+console.log(3
+    + 3 ) // code readability should be high
+
+console.log("rahul Chaudhary")
+
+let name = "rahul chaudhary" //string
+let age = 22 //number
+let isloggedIn = false //boolean
+
+// number => 2 to  power 53
+// bigint
+// sting => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not asigned
+// symbol => use to find unique ness 
+
+
+// object 
+
+
+console.log(typeof "t"); //string
+console.log(typeof 4); //number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
