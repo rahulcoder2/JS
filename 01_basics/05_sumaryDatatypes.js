7c1a77ca6d57ec7f5746ded1a57596ba318d2959
Rahul Chaudhary <91676034+rahulcoder2@users.noreply.github.com>
2023-12-29 15:15:54 +0000

    summary

---
+++ 01_basics/05_sumaryDatatypes.js
@@ -0,0 +1,27 @@
+/*
+    Datatype are two type primitive and Non-Primitive 
+*/
+
+
+/*
+// # Primitive (Call by value)
+
+    7 type :
+        1. String
+        2. Number
+        3. Boolean
+        4. Null
+        5. Undefined
+        6. Symbol
+        7. BigInt
+*/
+
+/*
+// # Non-Primitive (Call by reference)
+    their reference value can be allocated to you in memory
+
+    3 type :
+        1. Object
+        2. Array
+        3. Function
+*/
