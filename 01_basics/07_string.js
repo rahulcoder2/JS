03da95c1ee41a70b00ed2b67622a336001fbb85f
Rahul Chaudhary <91676034+rahulcoder2@users.noreply.github.com>
2023-12-30 04:21:29 +0000

    string

---
+++ 01_basics/07_string.js
@@ -0,0 +1,24 @@
+// old way to concat string 
+
+const name = "Rahul"
+let repoCount = 16
+
+// console.log(name + repoCount + "value")
+
+// New Way to concat string 
+
+// console.log(`hello my name is ${name}. my repoCount is ${repoCount} `)
+
+const gameName = new String("ClashofClan")
+
+// console.log(gameName)
+// console.log(`using key value access ${gameName[5]}`);
+// console.log(`accesing prototype object ${gameName.__proto__}`);
+
+                //Prototype call
+// console.log(gameName.length); // show length 11
+// console.log(gameName.charAt(4)) //show char at Index key 4 = h
+// console.log(gameName.toUpperCase()) // show all word in uppercase CLASHOFCLAN
+// console.log(gameName.fontcolor("blue")) //change font color
+// console.log(gameName.indexOf('a', 5)) // fInd index key using char and postion number // ans 9
+
