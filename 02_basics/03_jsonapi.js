3089a5c29df2c6f12b2be33b77143509535109df
Rahul Chaudhary <91676034+rahulcoder2@users.noreply.github.com>
2024-01-04 12:51:16 +0000

    Json api done

---
+++ 02_basics/03_jsonapi.js
@@ -0,0 +1,32 @@
+// Json in Js 
+  
+// Json file is write by using .json
+
+// this is example of JSON structure
+
+// {
+//     "squadName": "Super hero squad",
+//     "homeTown": "Metro City",
+//     "formed": 2016,
+//     "secretBase": "Super tower",
+//     "active": true,
+//     "members": [
+//       {
+//         "name": "Molecule Man",
+//         "age": 29,
+//         "secretIdentity": "Dan Jukes",
+//         "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+//       },
+//     ]
+//   }
+
+[
+    {},
+    {},
+]
+
+// how to access api in js
+//  Go to :
+// https://jsonformatter.org/
+
+// paste https://randomuser.me/api/ vaue one side and click CODE TO TREE   
