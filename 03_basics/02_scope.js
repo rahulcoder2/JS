fa91afbb6d05914416ef91624d9cc5113316e20f
Rahul Chaudhary <91676034+rahulcoder2@users.noreply.github.com>
2024-01-07 03:14:53 +0000

    scope done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -20,10 +20,64 @@ if (true) {
   const scope2 = 2;
     // scope3 = 3; so not to use 
 
-    console.log("inside scope or Local Scope = ", scope1)
+    // console.log("inside scope or Local Scope = ", scope1)
 } //bLock scope  
 
 // let check scope 
- console.log("global=" ,scope1) //ok
+//  console.log("global=" ,scope1) //ok
 // console.log(scope2) // ok
 // console.log(scope3) // not ok it run ///ans 3
+
+//nested scope 
+/*
+Note: this is also called closure
+*/
+//example
+
+function one (){
+  const userName ="rahul"
+
+  function two (){
+    const userEmail = "example@gmail.com"
+    console.log("Parent variable value= ",userName) // can access parent variable
+  }
+  // console.log(userEmail) // can't access child variable
+  two() // executed function two
+
+}
+
+ one() //executed function one
+
+  // let see in if 
+
+  if(true){
+    const fName = "rahul "
+    if(fName === "rahul "){
+      const lName = "Chaudhary"
+      console.log(fName + lName)
+    }
+    // console.log(lName) // can't access from child 
+  }
+  // console.log(fName) // can't access out side scope 
+
+  //now let see which way can your function can access data and not let see
+
+
+  console.log("before: ",tryingToAccessfromdirectfunction(5))
+
+  function tryingToAccessfromdirectfunction (num){
+    console.log("direct function")
+    return(num)
+  }
+  console.log("after : ",tryingToAccessfromdirectfunction(6))
+
+
+
+  // console.log("before: ",tryingToAccessFromVariableUsingFunction(8)) // can't access value 
+const tryingToAccessFromVariableUsingFunction = function (num){
+  console.log("using variable ")
+  return num
+}
+console.log("after: ",tryingToAccessFromVariableUsingFunction(8)) //can access value
+
+  
