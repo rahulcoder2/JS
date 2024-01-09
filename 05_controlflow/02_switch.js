0e45384226d119a001966a878ad25151a5b1cb93
Rahul Chaudhary <91676034+rahulcoder2@users.noreply.github.com>
2024-01-09 15:22:41 +0000

    switch done

---
+++ 05_controlflow/02_switch.js
@@ -0,0 +1,68 @@
+// switch
+
+// let delare switch and access it
+
+// switch( key){
+//     case 1:
+//         break;
+//         default:
+//             break:
+// }
+
+const week = 3;
+
+switch (week) {
+  case 1:
+    console.log("sun");
+    break;
+  case 2:
+    console.log("Mon");
+    break;
+  case 3:
+    console.log("Tue");
+    break;
+  case 4:
+    console.log("Wed");
+    break;
+  case 5:
+    console.log("Thu");
+    break;
+  case 5:
+    console.log("Fri");
+
+  case 6:
+    console.log(Sat);
+  default:
+    console.log("plz enter correct value");
+    break;
+}
+
+// using string as value
+
+const weeks = "sun";
+
+switch (weeks) {
+  case "sun":
+    console.log("sun");
+    break;
+  case "mon":
+    console.log("Mon");
+    break;
+  case "Tue":
+    console.log("Tue");
+    break;
+  case "Wed":
+    console.log("Wed");
+    break;
+  case "Thu":
+    console.log("Thu");
+    break;
+  case "Fri":
+    console.log("Fri");
+
+  case "Sat":
+    console.log(Sat);
+  default:
+    console.log("plz enter correct value");
+    break;
+}
